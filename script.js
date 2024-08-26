2d83ccbe34a4caf4f4fa94f1e18841b3993f4045
tanishq-git <tanishqsahu341@gmail.com>
2024-08-26 14:08:39 +0800

    add

---
+++ script.js
@@ -0,0 +1,55 @@
+let userdate = document.getElementById("date");
+ userdate.max = new Date().toISOString().split("T")[0];
+
+let result = document.querySelector("#result");
+
+let btn = document.querySelector("#button");
+btn.addEventListener("click",() => {
+
+    let birthdate = new Date(userdate.value);
+
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+
+    let today = new Date(); 
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+
+    let d3,m3,y3;
+
+   y3 = y2 - y1;
+   if(m2 >= m1){
+    m3 = m2 - m1;
+   }
+   else{
+    y3--;
+    m3 = 12 + m2 - m1;
+   }
+   if(d2 >= d1){
+    d3 = d2 - d1;
+   }
+   else{
+    m1--;
+    d3 = getdateandmonth(y1,m1) + d2 - d1;
+   }
+   if(m3 < 0){
+    m3 = 11;
+    y3--;
+   }
+   
+   result.innerHTML = `you are ${y3} years and ${m3} months and ${d3} days <br> <span>thank u!<span/>`
+  
+})
+
+
+ 
+   function getdateandmonth(year,month){
+    return new Date(year,month,0).getDate();
+   }
+   
+   
+
